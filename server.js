2226711d67b3ff4838e665914101a04198ec68ab
rashedabir <rashed15-2155@diu.edu.bd>
2021-07-28 21:15:25 +0600

    commit

--- server.js
+++ server.js
@@ -30,6 +30,13 @@ mongoose.connect(
   }
 );
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(PORT, () => {
   console.log(`Server is Connected to PORT ${PORT}`);
 });
