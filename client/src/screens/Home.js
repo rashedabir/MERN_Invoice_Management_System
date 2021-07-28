c0e07e4954e45a0ecb961cc64d7d00635aa351a1
rashedabir <rashed15-2155@diu.edu.bd>
2021-07-28 21:11:26 +0600

    commit

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -7,7 +7,7 @@ function Home() {
     <div className="home">
       <img src={home} alt="" />
       <div className="home_details">
-        <h3>sevDesk - Hassle-free Accounting!</h3>
+        <h3>Entkreis - Hassle-free Accounting!</h3>
         <p>Create invoices & estimates and automate your accounting</p>
         <Link to="/register" className="btn btn-primary">
           Get Started
